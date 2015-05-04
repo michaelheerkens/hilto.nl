c5e0c6202eaa099b11204546de6cbfa8a5b9bb81
Michael Heerkens <michael.heerkens@printocean.com>
2015-05-04 20:31:03 +0200

    fixes

--- site/config/config.php
+++ site/config/config.php
@@ -20,8 +20,11 @@ c::set('lang.support', true);
 c::set('lang.default', 'en');
 c::set('lang.available', array('en', 'de'));
 c::set('lang.detect', true);
-c::set('email.use', 'postmark');
+c::set('email.use', 'mailgun');
 c::set('email.postmark.key', 'ac89bdf1-7d2e-46ba-a9a7-5b9cb670fb71');
+c::set('email.mailgun.key', 'key-6cddd9a652b4c975f130078ac2faf99d');
+c::set('email.mailgun.domain', 'allblue.nl');
+
 //c::set('debug', true);
 /*
 
